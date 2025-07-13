9a03b1585790547cde93f7a21346aea1da731910
Felipe Baltor <fbaltor@gmail.com>
2025-07-13 04:01:01 -0300

    feat: js/ts/deno lsp working

--- init.lua
+++ init.lua
@@ -697,7 +697,14 @@ require('lazy').setup({
           --    https://github.com/pmizio/typescript-tools.nvim
           --
           -- But for many setups, the LSP (`ts_ls`) will work just fine
-          -- ts_ls = {},
+          ts_ls = {
+            workspace_required = true,
+            root_markers = { 'tsconfig.json' },
+          },
+          denols = {
+            workspace_required = true,
+            root_markers = { 'deno.json' },
+          },
           --
           lua_ls = {
             -- cmd = { ... },
